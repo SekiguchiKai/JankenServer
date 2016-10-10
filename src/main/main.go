989cc07ff35fd96033032dcf98d459dca0670b70
関口　海 <sekiguchikai@sekiguchikai-no-MacBook-Pro-2.local>
2016-10-10 20:12:53 +0900

    addいろいろ

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("a")
+//templateで使用するためのデータ型を作成する
+type JankenHTML struct {
+	Uchite string
+	Shouhai string
 }
+
+//HTMLをパースするための関数
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+	jankenHTML := JankenHTML{Uchite:"グー", Shouhai:"勝ち!"}
+
+	//index.htmlをパースする。
+	templ, err := template.ParseFiles("../template/index.html")
+
+	if err != nil {
+		fmt.Println(err)//エラー処理
+	}
+
+	/*template.Executeは、エラーがなければ、パースしたtemplateを指定したデータオブジェクトにして、
+	w http.ResponseWriterに出力してくれる。エラーが起きれば、戻り値の値が入る
+	 */
+	err = templ.Execute(w, jankenHTML)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+
+
+//formを受け取り、anken_model.goに処理をさせるメソッド
+func ProcessJudge(r *http.Request) string{
+	//POSTのときはリクエストのボディをフォームとして解析する
+	//r.Form（http.Request.Formに値が格納される）
+	//戻り値は、err
+	//エラー確認
+	err := r.ParseForm();
+	if err !=nil {
+		fmt.Println(err)
+	}
+	//clientFormの構造体のclientUchiteに代入
+	//型変換
+	ClientUchite := int(r.Form)
+
+	//クライアントの打ち手を引数に渡して、Janken.judgeGameに勝敗を決めさせる
+	result := Janken.JudgeGame(*ClientUchite)
+}
+
+
+
+
+func main() {
+	//"/"に対して、リクエストハンドラであるhtmlHandlerをバンドル
+	http.HandleFunc("/", htmlHandler)
+
+	//8080ポートをリッスンするようにする=>クライアントからの8080への接続を待ち受けている
+	http.ListenAndServe(":8080", nil)
+
+
+	//form入力後に表示させる"/Result"に対して、リクエストハンドラであるhtmlHandlerをバンドル
+	http.HandleFunc("/Result", ProcessJudge)
+
+
+}
+
